2cacd81f53a2e2d49e7c5502fadc76fee88728ed
palilingm <mpal0010@student.monash.edu>
2021-08-16 21:35:03 +0800

    test

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Test');
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+//key-value
+app.set('PORT', 8080);
+
+app.listen(app.get('PORT'), () => {
+    console.log(`We are listening on port ${app.get('PORT')}`);
+});
